36e581078114950ae78f3ee2846c028f2d96d3ae
Ma3ll3 <falch.maelle@gmail.com>
2023-10-04 00:06:09 +0200

    sfgbhn

--- js/CalculPourcentages.js
+++ js/CalculPourcentages.js
@@ -1,9 +1,9 @@
 import GameManager from "./GameManager.js";
 
-export default class CalculPourcentages{
+export default class CalculPourcentages {
 
-    constructor(){
-        this.gameManager = new GameManager(); 
+    constructor() {
+        this.gameManager = new GameManager();
         this.changementScene = this.gameManager.previousChangementScene;
         this.type = this.gameManager.type;
         this.jaugeBleu = 0;
@@ -11,101 +11,94 @@ export default class CalculPourcentages{
         this.jaugeVert = 0;
         this.combo = false;
         this.comboBuffer = [];
-        this.suite = 
-        [["Rouge","Rouge","Rouge","Rouge"],
-        ["Vert","Vert","Vert"],
-        ["Rouge","Rouge","Rouge"],
-        ["Bleu","Bleu","Bleu"],];
+        this.suite = [
+            ["Rouge", "Rouge", "Rouge", "Rouge"],
+            ["Vert", "Vert", "Vert"],
+            ["Rouge", "Rouge", "Rouge"],
+            ["Bleu", "Bleu", "Bleu"],
+        ];
         this.combos = {
-            1 : 
-            {
-                pourcentages : [25,30,15,5,25],
-                combinaison : 5,
-            },   
-            2 :
-            {   
-                pourcentages : [25,30,15,5,25],
-                combinaison : 5,
+            1: {
+                pourcentages: [25, 30, 15, 5, 25],
+                combinaison: 5,
             },
-            3 : 
-            {
-                pourcentages : [10,30,20,10,30],
-                combinaison : 2,
-            },  
-            4 : 
-            {
-                pourcentages : [55,10,10,5,20],
-                combinaison : 2,
-            } 
-            
+            2: {
+                pourcentages: [25, 30, 15, 5, 25],
+                combinaison: 5,
+            },
+            3: {
+                pourcentages: [10, 30, 20, 10, 30],
+                combinaison: 2,
+            },
+            4: {
+                pourcentages: [55, 10, 10, 5, 20],
+                combinaison: 2,
+            }
+
         }
 
         this.combinaisons = {
-            1 : {
-                    suite : ["Mid1", "Ecolo1", "Fascho1"],
-                    pourcentage : 0,
-                },
-            2 : { 
-                    suite : ["Fascho1" , "Fascho1" , "Mid1"],
-                    pourcentage : 0,
-                },   
-            3 : {
-                    suite : ["Mid1" ,"Mid1" , "Ecolo1"],
-                    pourcentage : 0,
-                },   
-            4 : {
-                    suite : ["Ecolo1","Ecolo1", "Mid1"],
-                    pourcentage : 0,
-                },
-            5 : {
-                    suite : ["Fascho1","Fascho1", "Fascho1"],
-                    pourcentage : 0, 
-                }
-                
+            1: {
+                suite: ["Mid1", "Ecolo1", "Fascho1"],
+                pourcentage: 0,
+            },
+            2: {
+                suite: ["Fascho1", "Fascho1", "Mid1"],
+                pourcentage: 0,
+            },
+            3: {
+                suite: ["Mid1", "Mid1", "Ecolo1"],
+                pourcentage: 0,
+            },
+            4: {
+                suite: ["Ecolo1", "Ecolo1", "Mid1"],
+                pourcentage: 0,
+            },
+            5: {
+                suite: ["Fascho1", "Fascho1", "Fascho1"],
+                pourcentage: 0,
+            }
+
         }
 
     }
 
-    Add_to_buffer(type){
+    Add_to_buffer(type) {
         this.comboBuffer.unshift(type);
         console.log(this.comboBuffer);
-        
+
     }
 
-    CheckBuffer(){
-        if(this.comboBuffer[0] === this.suite[0][0]&&this.comboBuffer[1] === this.suite[0][1]&&
-            this.comboBuffer[2] === this.suite[0][2]&&this.comboBuffer[3] === this.suite[0][3]){
+    CheckBuffer() {
+        if (this.comboBuffer[0] === this.suite[0][0] && this.comboBuffer[1] === this.suite[0][1] &&
+            this.comboBuffer[2] === this.suite[0][2] && this.comboBuffer[3] === this.suite[0][3]) {
             this.combo = true;
-            this.setPourcentages(this.combos[1].pourcentages[0],this.combos[1].pourcentages[1],
-                this.combos[1].pourcentages[2],this.combos[1].pourcentages[3],this.combos[1].pourcentages[4]);
+            this.setPourcentages(this.combos[1].pourcentages[0], this.combos[1].pourcentages[1],
+                this.combos[1].pourcentages[2], this.combos[1].pourcentages[3], this.combos[1].pourcentages[4]);
             this.comboBuffer = [];
-            console.log("combo1");
             this.gameManager.mort = true;
             return this.combinaisons[this.combos[1].combinaison].suite;
         }
-        if(this.comboBuffer[0] === this.suite[1][0]&&this.comboBuffer[1] === this.suite[1][1]&&
-            this.comboBuffer[2] === this.suite[1][2]){
+        if (this.comboBuffer[0] === this.suite[1][0] && this.comboBuffer[1] === this.suite[1][1] &&
+            this.comboBuffer[2] === this.suite[1][2]) {
             this.combo = true;
-            this.setPourcentages(this.combos[2].pourcentages[0],this.combos[2].pourcentages[2],
-                this.combos[2].pourcentages[2],this.combos[2].pourcentages[3],this.combos[2].pourcentages[4]);
-            console.log("combo2");
+            this.setPourcentages(this.combos[2].pourcentages[0], this.combos[2].pourcentages[2],
+                this.combos[2].pourcentages[2], this.combos[2].pourcentages[3], this.combos[2].pourcentages[4]);
             this.comboBuffer = [];
             return this.combinaisons[this.combos[2].combinaison].suite;
         }
-        if(this.comboBuffer[0] === this.suite[2][0]&&this.comboBuffer[1] === this.suite[2][1]&&
-            this.comboBuffer[2] === this.suite[2][2]){
+        if (this.comboBuffer[0] === this.suite[2][0] && this.comboBuffer[1] === this.suite[2][1] &&
+            this.comboBuffer[2] === this.suite[2][2]) {
             this.combo = true;
-            this.setPourcentages(this.combos[3].pourcentages[0],this.combos[3].pourcentages[3],
-                this.combos[3].pourcentages[3],this.combos[3].pourcentages[3],this.combos[3].pourcentages[4]);
-            console.log("combo3");
+            this.setPourcentages(this.combos[3].pourcentages[0], this.combos[3].pourcentages[3],
+                this.combos[3].pourcentages[3], this.combos[3].pourcentages[3], this.combos[3].pourcentages[4]);
             return this.combinaisons[this.combos[3].combinaison].suite;
         }
-        if(this.comboBuffer[0] === this.suite[3][0]&&this.comboBuffer[1] === this.suite[3][1]&&
-            this.comboBuffer[2] === this.suite[3][2]){
+        if (this.comboBuffer[0] === this.suite[3][0] && this.comboBuffer[1] === this.suite[3][1] &&
+            this.comboBuffer[2] === this.suite[3][2]) {
             this.combo = true;
-            this.setPourcentages(this.combos[4].pourcentages[0],this.combos[4].pourcentages[4],
-                this.combos[4].pourcentages[4],this.combos[4].pourcentages[3],this.combos[4].pourcentages[4]);
-            console.log("combo4");
+            this.setPourcentages(this.combos[4].pourcentages[0], this.combos[4].pourcentages[4],
+                this.combos[4].pourcentages[4], this.combos[4].pourcentages[3], this.combos[4].pourcentages[4]);
             this.comboBuffer = [];
             return this.combinaisons[this.combos[4].combinaison].suite;
         }
@@ -114,7 +107,7 @@ export default class CalculPourcentages{
         return null;
     }
 
-    initCombinaison(){
+    initCombinaison() {
         this.combinaisons[1].pourcentage = 100;
         this.combinaisons[2].pourcentage = 0;
         this.combinaisons[3].pourcentage = 0;
@@ -124,17 +117,17 @@ export default class CalculPourcentages{
         this.gameManager.scene2.style.display = 'block'
     }
 
-    Add_Vert(){
+    Add_Vert() {
         this.jaugeVert += 1;
     }
-    Add_Rouge(){
+    Add_Rouge() {
         this.jaugeRouge += 1;
-    }    
-    Add_Bleu(){
+    }
+    Add_Bleu() {
         this.jaugeBleu += 1;
     }
 
-    setPourcentages(nb1,nb2,nb3,nb4,nb5){
+    setPourcentages(nb1, nb2, nb3, nb4, nb5) {
         this.combinaisons[1].pourcentage = nb1;
         this.combinaisons[2].pourcentage = nb2;
         this.combinaisons[3].pourcentage = nb3;
@@ -145,30 +138,30 @@ export default class CalculPourcentages{
         }
     };
 
-    Calcul_Vert(){
+    Calcul_Vert() {
         this.combinaisons[2].pourcentage += 20;
         this.combinaisons[3].pourcentage += 10;
         this.combinaisons[4].pourcentage += 5;
         this.combinaisons[5].pourcentage += 5;
-        
+
         var sum = this.combinaisons[2].pourcentage + this.combinaisons[3].pourcentage + this.combinaisons[4].pourcentage + this.combinaisons[5].pourcentage;
 
-        if( sum >= 100){
+        if (sum >= 100) {
             this.combinaisons[1].pourcentage = 5;
         }
 
-        if( sum < 100){
+        if (sum < 100) {
             this.combinaisons[1].pourcentage = 100 - sum;
         }
 
         var sumTotal = sum + this.combinaisons[1].pourcentage;
-        if(sumTotal > 100){
+        if (sumTotal > 100) {
             for (let i = 1; i <= 5; i++) {
-                this.combinaisons[i].pourcentage = (this.combinaisons[i].pourcentage*100)/sum;
+                this.combinaisons[i].pourcentage = (this.combinaisons[i].pourcentage * 100) / sum;
                 console.log(this.combinaisons[i].pourcentage);
             }
         }
-        if (sumTotal < 100){
+        if (sumTotal < 100) {
             console.log('alerte, changez dans combinaisons');
         }
 
@@ -177,29 +170,29 @@ export default class CalculPourcentages{
         }
     }
 
-    Calcul_Rouge(){
+    Calcul_Rouge() {
         this.combinaisons[2].pourcentage += 30;
         this.combinaisons[3].pourcentage += 15;
         this.combinaisons[4].pourcentage += 5;
         this.combinaisons[5].pourcentage += 10;
-        
+
         var sum = this.combinaisons[2].pourcentage + this.combinaisons[3].pourcentage + this.combinaisons[4].pourcentage + this.combinaisons[5].pourcentage;
 
-        if( sum >= 100){
+        if (sum >= 100) {
             this.combinaisons[1].pourcentage = 5;
         }
-        if( sum < 100){
+        if (sum < 100) {
             this.combinaisons[1].pourcentage = 100 - sum;
         }
 
         var sumTotal = sum + this.combinaisons[1].pourcentage;
-        if(sumTotal > 100){
+        if (sumTotal > 100) {
             for (let i = 1; i <= 5; i++) {
-                this.combinaisons[i].pourcentage = (this.combinaisons[i].pourcentage*100)/sum;
+                this.combinaisons[i].pourcentage = (this.combinaisons[i].pourcentage * 100) / sum;
                 console.log(this.combinaisons[i].pourcentage);
             }
         }
-        if (sumTotal < 100){
+        if (sumTotal < 100) {
             console.log('alerte, changez dans combinaisons');
         }
 
@@ -208,29 +201,29 @@ export default class CalculPourcentages{
         }
     }
 
-    Calcul_Bleu(){
+    Calcul_Bleu() {
         this.combinaisons[2].pourcentage += 20;
         this.combinaisons[3].pourcentage += 20;
         this.combinaisons[4].pourcentage += 5;
         this.combinaisons[5].pourcentage += 5;
-        
+
         var sum = this.combinaisons[2].pourcentage + this.combinaisons[3].pourcentage + this.combinaisons[4].pourcentage + this.combinaisons[5].pourcentage;
 
-        if( sum >= 100){
+        if (sum >= 100) {
             this.combinaisons[1].pourcentage = 5;
         }
-        if( sum < 100){
+        if (sum < 100) {
             this.combinaisons[1].pourcentage = 100 - sum;
         }
 
         var sumTotal = sum + this.combinaisons[1].pourcentage;
-        if(sumTotal > 100){
+        if (sumTotal > 100) {
             for (let i = 1; i <= 5; i++) {
-                this.combinaisons[i].pourcentage = (this.combinaisons[i].pourcentage*100)/sum;
+                this.combinaisons[i].pourcentage = (this.combinaisons[i].pourcentage * 100) / sum;
                 console.log(this.combinaisons[i].pourcentage);
             }
         }
-        if (sumTotal < 100){
+        if (sumTotal < 100) {
             console.log('alerte, changez dans combinaisons');
         }
 
@@ -239,7 +232,7 @@ export default class CalculPourcentages{
         }
     }
 
-    Get_combinaisons(){
+    Get_combinaisons() {
         var randomInt = Math.random() * 100;
         // var randomInt = 0;
         var interval0 = 0;
@@ -248,30 +241,30 @@ export default class CalculPourcentages{
         var interval3 = this.combinaisons[1].pourcentage + this.combinaisons[2].pourcentage + this.combinaisons[3].pourcentage;
         var interval4 = this.combinaisons[1].pourcentage + this.combinaisons[2].pourcentage + this.combinaisons[3].pourcentage + this.combinaisons[4].pourcentage;
         var interval5 = this.combinaisons[1].pourcentage + this.combinaisons[2].pourcentage + this.combinaisons[3].pourcentage + this.combinaisons[4].pourcentage + this.combinaisons[5].pourcentage;
-    
+
         console.log('random' + randomInt);
 
-        if(interval0 <= randomInt && randomInt < interval1){
+        if (interval0 <= randomInt && randomInt < interval1) {
             console.log('interval 1 ' + interval1);
             return this.combinaisons[1].suite;
         }
-        if(interval1 <= randomInt && randomInt < interval2){
+        if (interval1 <= randomInt && randomInt < interval2) {
             console.log("interval 2");
             return this.combinaisons[2].suite;
         }
-        if(interval2 <= randomInt && randomInt < interval3){
+        if (interval2 <= randomInt && randomInt < interval3) {
             console.log("interval 3");
             return this.combinaisons[3].suite;
         }
-        if(interval3 <= randomInt && randomInt < interval4){
+        if (interval3 <= randomInt && randomInt < interval4) {
             console.log("interval 4");
             return this.combinaisons[4].suite;
         }
-        if(interval4 <= randomInt && randomInt < interval5){
+        if (interval4 <= randomInt && randomInt < interval5) {
             console.log("interval 5");
             return this.combinaisons[5].suite;
         }
-        if(randomInt == 100){
+        if (randomInt == 100) {
             return this.combinaisons[5].suite;
         }
 
